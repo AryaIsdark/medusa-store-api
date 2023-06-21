4a00989ad9dcfba425cfc20bef303421235edcb1
Arya <ars@bankingcircle.com>
2023-06-21 21:46:35 +0200

    enabling admin panel

--- medusa-config.js
+++ medusa-config.js
@@ -43,13 +43,14 @@ const plugins = [
     },
   },
   // To enable the admin plugin, uncomment the following lines and run `yarn add @medusajs/admin`
-  // {
-  //   resolve: "@medusajs/admin",
-  //   /** @type {import('@medusajs/admin').PluginOptions} */
-  //   options: {
-  //     autoRebuild: true,
-  //   },
-  // },
+  {
+    resolve: "@medusajs/admin",
+    /** @type {import('@medusajs/admin').PluginOptions} */
+    options: {
+      autoRebuild: true,
+      path: "backoffice",
+    },
+  },
 ];
 
 const modules = {
