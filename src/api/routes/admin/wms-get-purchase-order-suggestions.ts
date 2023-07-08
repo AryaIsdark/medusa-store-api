0048096a47d57508f091b00aae07f4e26c086ca5
Arya <ars@bankingcircle.com>
2023-07-08 23:49:57 +0200

    improved functionality and code readability

--- src/api/routes/admin/wms-get-purchase-order-suggestions.ts
+++ src/api/routes/admin/wms-get-purchase-order-suggestions.ts
@@ -1,15 +1,12 @@
 import { Request, Response } from "express";
 import { Order, Article } from "models/wms/types";
 
-const getSuggestedArticleQuanitityToPurchase = (
+const getSuggestedArticleQuantityToPurchase = (
   quantityInStock,
   quantityInOrders
 ) => {
   const diff = quantityInStock - quantityInOrders;
-  if (diff < 0) {
-    // if fx diff is -5 return 5 items to buy
-    return Math.abs(diff);
-  }
+  return diff < 0 ? Math.abs(diff) : 0;
 };
 
 const getArticleQuantityInWarehouse = (inventoryPerWarehouse) => {
@@ -18,30 +15,31 @@ const getArticleQuantityInWarehouse = (inventoryPerWarehouse) => {
   }, 0);
 };
 
-const getAllArticlesInOrders = (orders: Order[]) => {
-  const articles: Article[] = [];
+const getAllArticlesInOrders = (orders) => {
+  const articles = [];
   const articleNumbers = new Set();
 
-  orders.forEach((order) => {
-    order.orderLines.forEach((orderLine) => {
-      if (orderLine.article) {
-        articles.push(orderLine.article);
-        articleNumbers.add(orderLine.article.articleNumber);
+  for (const order of orders) {
+    for (const orderLine of order.orderLines) {
+      const article = orderLine.article;
+      if (article) {
+        articles.push(article);
+        articleNumbers.add(article.articleNumber);
       }
-    });
-  });
+    }
+  }
 
   return articles;
 };
 
-const getArticleName = (articles: Article[], articleInventory) => {
+const getArticleName = (articles, articleInventory) => {
   const article = articles.find(
     (article) => article.articleNumber === articleInventory.articleNumber
   );
   return article ? article.articleName : "";
 };
 
-export default async (req: Request, res: Response) => {
+export default async (req, res) => {
   const wmsService = req.scope.resolve("wmsService");
   const orders = await wmsService.getOrders();
   const articles = getAllArticlesInOrders(orders.data);
@@ -49,19 +47,27 @@ export default async (req: Request, res: Response) => {
   const articlesInventoryPerWarehouse =
     await wmsService.getArticleInventoryPerWarehouse(articleNumbers);
 
-  const data = articlesInventoryPerWarehouse.map((articleInventory) => ({
-    productReference: articleInventory.articleNumber,
-    productQuantity: getSuggestedArticleQuanitityToPurchase(
-      getArticleQuantityInWarehouse(articleInventory.inventoryPerWarehouse),
-      articleNumbers.filter(
-        (articleNumber) => articleNumber === articleInventory.articleNumber
-      ).length
-    ),
-    productName: getArticleName(articles, articleInventory),
-    stock: getArticleQuantityInWarehouse(
-      articleInventory.inventoryPerWarehouse
-    ),
-  }));
+  const data = articlesInventoryPerWarehouse
+    .map((articleInventory) => {
+      const productQuantity = getSuggestedArticleQuantityToPurchase(
+        getArticleQuantityInWarehouse(articleInventory.inventoryPerWarehouse),
+        articleNumbers.filter(
+          (articleNumber) => articleNumber === articleInventory.articleNumber
+        ).length
+      );
+      const productName = getArticleName(articles, articleInventory);
+      const stock = getArticleQuantityInWarehouse(
+        articleInventory.inventoryPerWarehouse
+      );
+
+      return {
+        productReference: articleInventory.articleNumber,
+        productQuantity,
+        productName,
+        stock,
+      };
+    })
+    .filter((item) => item.productQuantity > 0);
 
   res.json({
     status: 200,
