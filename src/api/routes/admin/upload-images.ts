a9ce235cd38a594fe5382b13a3da9adf47b819c4
Arya <ars@bankingcircle.com>
2023-07-10 09:03:44 +0200

    changed cloudinary config to secure

--- src/api/routes/admin/upload-images.ts
+++ src/api/routes/admin/upload-images.ts
@@ -9,6 +9,7 @@ cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
 });
 
 export default async (req: multer, res: Response): Promise<void> => {
