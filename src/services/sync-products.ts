44b94afad735e64a19c55b89a59c59ae3ec76d4b
Arya Shahali <shahali.arya@gmail.com>
2023-07-24 00:15:35 +0200

    Feature/update supplier product after variant creation (#3)
    * feat:
    
    * update
    
    * update
    
    ---------
    
    Co-authored-by: Arya <ars@bankingcircle.com>

--- src/services/sync-products.ts
+++ src/services/sync-products.ts
@@ -2,12 +2,10 @@ import {
   TransactionBaseService,
   ProductVariantService,
   ProductService,
-  ProductVariant,
-  Product,
   ShippingProfileService,
   SalesChannelService,
-  ShippingProfile,
-  SalesChannel,
+  ProductVariant,
+  Product,
 } from "@medusajs/medusa";
 import SupplierProductService from "./supplier-product";
 import { SupplierProduct } from "models/supplier-product";
@@ -15,16 +13,6 @@ import {
   prepareProductObj,
   prepareProductVarianObj,
 } from "../api/routes/admin/products/helpers/helpers";
-import { groupProductsByVariants } from "../api/routes/admin/supplier/get-variants";
-
-type ExtendedSupplierProduct = SupplierProduct & {
-  parentProduct: Product;
-};
-
-type CreateProductAndVariantInput = {
-  product: SupplierProduct;
-  variants: SupplierProduct[];
-};
 
 class SyncProductsService extends TransactionBaseService {
   private supplierProductService: SupplierProductService;
@@ -51,75 +39,23 @@ class SyncProductsService extends TransactionBaseService {
       this.salesChannelService.retrieveDefault();
   }
 
-  async mapVariants() {
-    const products = await this.productVariantService.list({});
-    console.log("products", products);
-    const supplierProducts = await this.supplierProductService.list();
-    for (const supplierProduct of supplierProducts) {
-      const variant = await this.productVariantService.retrieveBySKU(
-        supplierProduct.sku
+  private async updateSupplierProduct(supplierProduct: SupplierProduct) {
+    try {
+      await this.supplierProductService.update(supplierProduct.id, {
+        isCreatedInStore: true,
+      });
+    } catch (e) {
+      console.log(
+        "something went wrong when trying to update supplier product with id:",
+        supplierProduct.id,
+        e
       );
-
-      const parentId: string = variant.metadata.parentId as unknown as string;
-
-      console.log("variant", variant);
-      if (parentId) {
-        const variants = await this.supplierProductService.findByParentId(
-          parentId
-        );
-      }
-    }
-  }
-
-  async createProductTask() {
-    const [defaulShippingProfile, defaultSalesChannel] = await Promise.all([
-      this.defaulShippingProfilePromise,
-      this.defaultSalesChannelPromise,
-    ]);
-
-    const supplierProducts = await this.supplierProductService.list();
-
-    for (const supplierProduct of supplierProducts) {
-      try {
-        const newProduct = await this.productService.create(
-          prepareProductObj(
-            supplierProduct,
-            null,
-            defaulShippingProfile,
-            defaultSalesChannel
-          )
-        );
-
-        if (newProduct.id) {
-          const variantOptions = newProduct.options.map((v) => ({
-            option_id: v.id,
-            value: supplierProduct.productName,
-          }));
-          try {
-            await this.productVariantService.create(
-              newProduct.id,
-              prepareProductVarianObj(supplierProduct, variantOptions)
-            );
-          } catch (e) {
-            console.log(
-              `something went wrong when trying to create variant ${supplierProduct.reference} - ${supplierProduct.productName}`,
-              e
-            );
-          }
-        }
-      } catch (e) {
-        console.log(
-          `something went wrong when trying to create product ${supplierProduct.sku} - ${supplierProduct.productName}`,
-          e
-        );
-      }
     }
   }
 
   groupProductsByParentId(list) {
     const groupedProducts = list.reduce((acc, obj) => {
       const parentId = obj.parentId;
-
       // Check if a product with the same parentId exists in the accumulator
       const existingProduct = acc.find(
         (item) => item.productParentId === parentId
@@ -139,46 +75,29 @@ class SyncProductsService extends TransactionBaseService {
     return groupedProducts;
   }
 
-  private async addNewProductWithVariants(supplierProducts: SupplierProduct[]) {
+  private async createProduct(baseProduct: SupplierProduct) {
     const [defaulShippingProfile, defaultSalesChannel] = await Promise.all([
       this.defaulShippingProfilePromise,
       this.defaultSalesChannelPromise,
     ]);
 
+    return this.productService.create(
+      prepareProductObj(
+        baseProduct,
+        [],
+        defaulShippingProfile,
+        defaultSalesChannel
+      )
+    );
+  }
+
+  private async addNewProductWithVariants(supplierProducts: SupplierProduct[]) {
     if (supplierProducts.length) {
       const baseProduct = supplierProducts[0];
       try {
-        const newProduct = await this.productService.create(
-          prepareProductObj(
-            baseProduct,
-            [],
-            defaulShippingProfile,
-            defaultSalesChannel
-          )
-        );
-        console.log("new product added", newProduct);
+        const newProduct = await this.createProduct(baseProduct);
         if (newProduct.id) {
-          await Promise.all(
-            supplierProducts.map(async (variant) => {
-              const variantOptions = newProduct.options.map((v) => ({
-                option_id: v.id,
-                value: variant.productName,
-              }));
-
-              console.log("variant options", variantOptions);
-              try {
-                await this.productVariantService.create(
-                  newProduct.id,
-                  prepareProductVarianObj(variant, variantOptions)
-                );
-              } catch (e) {
-                console.log(
-                  `something went wrong when trying to create variant ${variant.sku} - ${variant.productName}`,
-                  e
-                );
-              }
-            })
-          );
+          await this.createVariantsForProduct(newProduct, supplierProducts);
         }
       } catch (e) {
         console.log(
@@ -189,14 +108,41 @@ class SyncProductsService extends TransactionBaseService {
     }
   }
 
+  private async createVariantsForProduct(
+    newProduct: Product,
+    supplierProducts: SupplierProduct[]
+  ) {
+    await Promise.all(
+      supplierProducts.map(async (supplierProduct) => {
+        const variantOptions = newProduct.options.map((v) => ({
+          option_id: v.id,
+          value: supplierProduct.productName,
+        }));
+        try {
+          const newVariant: ProductVariant =
+            await this.productVariantService.create(
+              newProduct.id,
+              prepareProductVarianObj(supplierProduct, variantOptions)
+            );
+          if (newVariant.id) {
+            await this.updateSupplierProduct(supplierProduct);
+          }
+        } catch (e) {
+          console.log(
+            `something went wrong when trying to create variant ${supplierProduct.reference} - ${supplierProduct.productName}`,
+            e
+          );
+        }
+      })
+    );
+  }
+
   async beginSync() {
     const supplierProducts = await this.supplierProductService.search({
       isCreatedInStore: false,
     });
 
-    const grouppedProducts = this.groupProductsByParentId(
-      supplierProducts.slice(0, 50)
-    );
+    const grouppedProducts = this.groupProductsByParentId(supplierProducts);
 
     const batchSize = 1; // Set the desired batch size
     const totalProducts = grouppedProducts.length;
@@ -211,8 +157,6 @@ class SyncProductsService extends TransactionBaseService {
       await Promise.all(
         batch.map(async (product) => {
           const productVariants = product.variants;
-          // const productImageUrlsTobeUploaded = getImages(productVariants);
-          // const imageUrls = await uploadImages(productImageUrlsTobeUploaded);
           await this.addNewProductWithVariants(productVariants);
         })
       );
