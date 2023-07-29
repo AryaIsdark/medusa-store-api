29b503c0c5a616da270bce5eeddcf7d9fb19967e
Arya <ars@bankingcircle.com>
2023-07-29 22:18:12 +0200

    removed isJobExecuted flag

--- src/loaders/sync-products.ts
+++ src/loaders/sync-products.ts
@@ -1,12 +1,6 @@
 import { AwilixContainer } from "awilix";
 
-const syncProductsJob = async (
-  container: AwilixContainer,
-  options: Record<string, any>
-) => {
-  // Add a flag to track if the job has already run
-  let isJobExecuted = false;
-
+const syncProductsJob = async (container: AwilixContainer) => {
   const jobSchedulerService = container.resolve("jobSchedulerService");
   jobSchedulerService.create(
     "sync-products-create-job",
@@ -14,26 +8,17 @@ const syncProductsJob = async (
     "0 0 * * 3", // The cron expression for running every Wednesday at 00:00 (midnight)
     async () => {
       try {
-        // Check if the job has already been executed
-        if (isJobExecuted) {
-          console.log("Job already executed. Skipping...");
-          return;
-        }
-
-        // Set the flag to true, indicating the job has run
-        isJobExecuted = true;
-
-        // Rest of your existing job logic goes here
+        // Your job logic goes here
         const supplierProductService = container.resolve(
           "supplierProductService"
         );
-        const newSupplilerProducts = await supplierProductService.search({
+        const newSupplierProducts = await supplierProductService.search({
           isCreatedInStore: false,
         });
         const syncProductsService = container.resolve("syncProductsService");
 
-        console.log("sync-products-job:17", newSupplilerProducts.length);
-        if (newSupplilerProducts.length) {
+        console.log("sync-products-job:17", newSupplierProducts.length);
+        if (newSupplierProducts.length) {
           await syncProductsService.beginCreateSync();
         }
       } catch (error) {
