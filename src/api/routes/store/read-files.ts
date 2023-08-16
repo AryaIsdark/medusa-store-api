17af56e250e6d41b0577a383589c19c74bd76eb7
Arya Shahali <shahali.arya@gmail.com>
2023-08-16 20:48:34 +0200

    added read file endpoint (#13)
    Co-authored-by: Arya <ars@bankingcircle.com>

---
+++ src/api/routes/store/read-files.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { processExcelFile } from "../admin/supplier/supplier-process-product-file";
+
+export default async (req: Request, res: Response): Promise<void> => {
+  const directory = "./product_downloads/products-test-file.xlsx";
+  const supplierService = req.scope.resolve("supplierService");
+
+  processExcelFile(directory)
+    .then(async (data) => {
+      const response = await supplierService.syncSupplierProducts(data);
+
+      res.json({
+        status: 200,
+        data: data,
+      });
+    })
+    .catch((error) => {
+      console.error(
+        "An error occurred while processing the Excel file:",
+        error
+      );
+      res.status(400);
+      res.json({
+        status: 400,
+        count: error.length,
+        validationErrors: error,
+      });
+    });
+};
