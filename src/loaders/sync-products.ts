a91e1514b28552703244ba39dbb5ef217341a063
Arya Sh <shahali.arya@gmail.com>
2023-08-21 07:06:16 +0200

    updated cron job time

--- src/loaders/sync-products.ts
+++ src/loaders/sync-products.ts
@@ -8,7 +8,7 @@ const SyncProductsJob = async (
     options: Record<string, any>
 ) => {
     const jobSchedulerService = container.resolve("jobSchedulerService");
-    jobSchedulerService.create("sync-products-cron-job", {}, "* */12 * * *", async () => {
+    jobSchedulerService.create("sync-products-cron-job", {}, "0 8/20 * * *", async () => {
         const supplierService = container.resolve("supplierService");
         const syncProductsService = container.resolve("syncProductsService");
 
